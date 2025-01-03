709c0d3e9edebbf88867abc2acd92898c9016dd5
SyedaRubaisha4 <syedarubaisha4@gmail.com>
2025-01-03 19:57:58 +0500

    ApplicationDbContext

---
+++ LibraryManagementSystem/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index( )
+        {
+            return View();
+        }
+        public IActionResult UpdateCategory()
+        {
+            return View();
+        }
+        public IActionResult ViewCategory()
+        {
+            return View();
+        }
+        public IActionResult DeleteCategory()
+        {
+            return View();
+        }
+       
+
+    }
+}
